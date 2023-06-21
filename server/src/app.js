9c99a8c7f8476f31f3f35c90cfdfba605d27dc65
shinnthant2004 <shinthantequi@gmail.com>
2023-06-21 11:25:11 +0630

    understanding cors

---
+++ server/src/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+const planetsRouter = require("./routes/planets/planets.route");
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+app.use(planetsRouter);
+
+module.exports = app;
