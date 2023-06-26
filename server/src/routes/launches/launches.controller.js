3a906d7df7bb038869594ee85be6a6ea56195986
shinnthant2004 <shinthantequi@gmail.com>
2023-06-26 16:17:52 +0630

    api validation

--- server/src/routes/launches/launches.controller.js
+++ server/src/routes/launches/launches.controller.js
@@ -7,6 +7,24 @@ function httpGetAllLaunches(req, res) {
 function httpAddNewLaunch(req, res) {
   let launch = req.body;
   launch.launchDate = new Date(launch.launchDate);
+
+  if (
+    !launch.mission ||
+    !launch.destination ||
+    !launch.launchDate ||
+    !launch.rocket
+  ) {
+    return res.status(400).json({
+      error: "Bad Request",
+    });
+  }
+
+  if (isNaN(launch.launchDate)) {
+    return res.status(400).json({
+      error: "Invalid Date",
+    });
+  }
+
   addNewLaunch(launch);
   res.status(201).json(launch);
 }
